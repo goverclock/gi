be697edfbee30c9f7abacabf16a2416efc643ccf
goverclock <goverclock@outlook.com>
2025-07-05 18:15:29 +0800

    line numbers
    Signed-off-by: goverclock <goverclock@outlook.com>

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # gi
 
 gi stands for good inspect. Its goal is to be notepad++'s terminal substitution.
-
-- [ ] open and view a log file
-
-- [ ] basic seperate search window
--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,25 @@
-use color_eyre::Result;
-use color_eyre::owo_colors::OwoColorize;
-use crossterm::event::{self, Event};
-use ratatui::text::{Line, Span};
-use ratatui::{DefaultTerminal, Frame, widgets::Paragraph};
+use color_eyre::{
+    Result,
+    eyre::{Ok, eyre},
+};
 use std::env;
-use std::fs;
 
-struct App {
-    file_paths: Vec<String>,
-    contents: String,
-}
+mod app;
+mod log_view;
 
-impl App {
-    fn new(file_paths: Vec<String>) -> Self {
-        Self {
-            file_paths,
-            contents: String::from("FUCK"),
-        }
+fn parse_cmd_args() -> Result<String> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        return Err(eyre!("bad cmd args, provide log file/directory path"));
     }
-
-    fn run(mut self, terminal: &mut DefaultTerminal) -> Result<()> {
-        let contents = fs::read_to_string(&self.file_paths[1]).expect("should be a file");
-        self.contents = contents;
-        loop {
-            terminal.draw(|frame| self.render(frame))?;
-            if matches!(event::read()?, Event::Key(_)) {
-                break Ok(());
-            }
-        }
-    }
-
-    fn render(&self, frame: &mut Frame) {
-        let p = Paragraph::new(self.contents.clone());
-
-        frame.render_widget(p, frame.area());
-    }
+    Ok(args[1].clone())
 }
 
 fn main() -> Result<()> {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        return Ok(());
-    }
-
     color_eyre::install()?;
+    let log_path = parse_cmd_args()?;
+
     let mut terminal = ratatui::init();
     terminal.clear().unwrap();
-    App::new(args).run(&mut terminal)
+    app::App::new(log_path).run(&mut terminal)
 }
